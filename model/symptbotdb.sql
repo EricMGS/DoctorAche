9dfb1d900ce4f239030127ffa4b74d212be314f6
Otávio Araújo <otavioajardim@gmail.com>
2018-09-20 18:35:27 -0300

    Criação da  tabela doenca_sintoma

--- model/symptbotdb.sql
+++ model/symptbotdb.sql
@@ -27,3 +27,14 @@ CREATE TABLE IF NOT EXISTS `sintomas` (
     CONSTRAINT `pk_sintoma` PRIMARY KEY (`id_sintoma`)
     
 )DEFAULT CHARSET = utf8;
+
+
+
+CREATE TABLE `doenca_sintoma` (
+  `id_doenca`    INT		NOT NULL,
+  `id_sintoma`   INT		NOT NULL,
+  
+  CONSTRAINT `id_doenca_sintoma`	PRIMARY KEY (`id_doenca`,`id_sintoma`),
+  CONSTRAINT `fk_doenca`          	FOREIGN KEY (`id_doenca`)           		REFERENCES `doencas` (`id_doenca`),
+  CONSTRAINT `fk_sintoma`      		FOREIGN KEY (`id_sintoma`)           		REFERENCES `sintomas` (`id_sintoma`)
+  );
