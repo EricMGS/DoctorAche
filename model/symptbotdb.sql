136180b1b296bbe3d8f6f691e24f8753df6b1be7
Otávio Araújo <otavioajardim@gmail.com>
2018-09-20 18:34:26 -0300

    Criação da tabela doencas

--- model/symptbotdb.sql
+++ model/symptbotdb.sql
@@ -5,3 +5,14 @@ DEFAULT COLLATE UTF8_GENERAL_CI;
 
 
 USE `symptbotdb`;
+
+
+
+CREATE TABLE IF NOT EXISTS `doencas` (
+
+	`id_doenca` INT 			NOT NULL AUTO_INCREMENT,
+    `nome` 		VARCHAR(250)  	NOT NULL,
+    
+    CONSTRAINT `pk_doenca` PRIMARY KEY (`id_doenca`)
+    
+)DEFAULT CHARSET = utf8;
