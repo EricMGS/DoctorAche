7c2d43088c2647cc9e2ff813bb90d54e88490265
ericmgs <37462627+EricMGS@users.noreply.github.com>
2018-09-23 00:02:47 -0300

    Delete codigo_exemplo.c

--- pasta_exemplo/codigo_exemplo.c
+++
@@ -1,7 +0,0 @@
-#include <exemplo.h>
-
-//Só pra exemplificar como os códigos podem ser vistos e mod DE 2 É 0?
-
-
-
-//ISTO FOI MODIFICADO NO NOVO BRANCH E SÓ VAI SER MESCLADO COM O CÓDIGO ORIGINAL QUANDO FOR APROVADA
