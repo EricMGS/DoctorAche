ff70f3b9b92dca45cec0c69b3ef5efbd1852d814
ericmgs <ericmgsouza@gmail.com>
2018-10-24 13:49:14 -0300

    Update scriptInsercaoBD.py

--- database/scriptInsercaoBD.py
+++ database/scriptInsercaoBD.py
@@ -5,14 +5,17 @@ conn = sqlite3.connect('database')
 c = conn.cursor()
 os.system('clear')
 
-print('INSERÇÃO DE DOENÇAS\n')
+print('INSERÇÃO DE SINTOMAS\n')
 
-nome = input('Digite 0 para sair ou\nDigite o nome da doença: ')
+opcao = int(input('Digite 0 para inserir doenças ou 1 para sintomas: '))
+opcao = ['doencas', 'sintomas'][opcao]
+
+nome = input('Digite 0 para sair ou\nDigite o nome: ')
 while nome != '0':
 	os.system('clear')
-	c.execute("insert into doencas values(null, '%s')" %nome)
+	c.execute("insert into %s values(null, '%s')" %(opcao,nome))
 	conn.commit()
 	os.system('clear')
-	nome = input('Digite 0 para sair ou\nDigite o nome da doença: ')
+	nome = input('Digite 0 para sair ou\nDigite o nome: ')
 
 c.close()
