637c9bd168c65a5fa65406fab17b7ad55c0a2f29
ericmgs <ericmgsouza@gmail.com>
2018-10-27 12:16:17 -0300

    Adicionada funcionalidade autocomplete ao combobox

--- frontend/teste.py
+++ frontend/teste.py
@@ -2,8 +2,54 @@
 
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt, QSortFilterProxyModel
+from PyQt5.QtWidgets import QCompleter, QComboBox
 sys.path.append('../backend/')
 from DoctorAche import DoctorAche as DA
+from spellChecker import spellChecker as SC
+
+class ExtendedComboBox(QComboBox):
+    def __init__(self, parent=None):
+        super(ExtendedComboBox, self).__init__(parent)
+
+        self.setFocusPolicy(Qt.StrongFocus)
+        self.setEditable(True)
+
+        # add a filter model to filter matching items
+        self.pFilterModel = QSortFilterProxyModel(self)
+        self.pFilterModel.setFilterCaseSensitivity(Qt.CaseInsensitive)
+        self.pFilterModel.setSourceModel(self.model())
+
+        # add a completer, which uses the filter model
+        self.completer = QCompleter(self.pFilterModel, self)
+        # always show all (filtered) completions
+        self.completer.setCompletionMode(QCompleter.UnfilteredPopupCompletion)
+        self.setCompleter(self.completer)
+
+        # connect signals
+        self.lineEdit().textEdited.connect(self.pFilterModel.setFilterFixedString)
+        self.completer.activated.connect(self.on_completer_activated)
+
+    # on selection of an item from the completer, select the corresponding item from combobox 
+    def on_completer_activated(self, text):
+        if text:
+            index = self.findText(text)
+            self.setCurrentIndex(index)
+            self.activated[str].emit(self.itemText(index))
+
+    # on model change, update the models of the filter and completer as well 
+    def setModel(self, model):
+        super(ExtendedComboBox, self).setModel(model)
+        self.pFilterModel.setSourceModel(model)
+        self.completer.setModel(self.pFilterModel)
+
+
+    # on model column change, update the model column of the filter and completer as well
+    def setModelColumn(self, column):
+        self.completer.setCompletionColumn(column)
+        self.pFilterModel.setFilterKeyColumn(column)
+        super(ExtendedComboBox, self).setModelColumn(column)  
+
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
@@ -17,6 +63,7 @@ class Ui_Dialog(object):
         icon.addPixmap(QtGui.QPixmap("img/BalaoFala.jpg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         Dialog.setWindowIcon(icon)
         Dialog.setStyleSheet("background-color: rgb(255, 255, 255);")
+
         self.btn_ok = QtWidgets.QPushButton(Dialog)
         self.btn_ok.setGeometry(QtCore.QRect(390, 390, 171, 51))
         font = QtGui.QFont()
@@ -24,9 +71,13 @@ class Ui_Dialog(object):
         font.setPointSize(9)
         self.btn_ok.setFont(font)
         self.btn_ok.setObjectName("btn_ok")
-        self.cmb_entrada = QtWidgets.QComboBox(Dialog)
+
+        self.cmb_entrada = ExtendedComboBox(Dialog)
         self.cmb_entrada.setGeometry(QtCore.QRect(50, 40, 241, 61))
         self.cmb_entrada.setObjectName("cmb_entrada")
+        self.cmb_entrada.addItems(doctor.lista_sintomas)
+        self.cmb_entrada.setCurrentText("")
+
         self.list_sintomas = QtWidgets.QListView(Dialog)
         self.list_sintomas.setGeometry(QtCore.QRect(350, 30, 291, 321))
         self.list_sintomas.setObjectName("list_sintomas")
