a88c8b6c39902a81e343df4f8ec86b206670fa15
ericmgs <ericmgsouza@gmail.com>
2018-11-04 20:25:52 -0200

    Corrigido bug em update.py

--- database/update.py
+++ database/update.py
@@ -1,57 +1,56 @@
 import sqlite3
 
-conn = sqlite3.connect('database')
-c = conn.cursor()
-c.execute('delete from doencas')
-c.execute('delete from sintomas')
-c.execute('delete from relacoes')
+database = sqlite3.connect('database')
+cursor = database.cursor()
 
-def inserirRelacao(doenca, sintoma):
-	c.execute("select id from doencas where nome='%s'" %doenca)
-	for linha in c:
-		id_doenca = linha[0]
+arquivo = open('doencas')
+doencas = []
+sintomas = []
+relacoes = []
 
-	c.execute("select id from sintomas where nome='%s'" %sintoma)
-	for linha in c:
-		id_sintoma = linha[0]
+linha = arquivo.readline() #NOME
+while linha != '':
+	linha = arquivo.readline().replace('\n','') #nome da doença
+	doencas.append([linha, 0])
 
-	c.execute("insert into relacoes values(null, %d, %d)" %(id_doenca, id_sintoma))
+	linha = arquivo.readline() #CASOS
+	linha = arquivo.readline().replace('\n','') #número de casos
+	doencas[-1][-1] = linha
 
-def abre_arquivo():
-	arquivo = open('doencas')
-	return arquivo
+	linha = arquivo.readline() #SINTOMAS
+	linha = arquivo.readline().replace('\n','') #primeiro sintoma
 
-def le_arquivo(arquivo):
-	doencas = []
-	sintomas = []
+	while '===' not in linha:
+		if linha not in sintomas:
+			sintomas.append(linha)
+
+		relacoes.append([doencas[-1][0], linha])
+
+		linha = arquivo.readline().replace('\n','') #próximos sintomas
 
 	linha = arquivo.readline() #NOME
-	while linha != '':
-		linha = arquivo.readline().replace('\n','') #nome de doença
-		doencas.append([linha, 0])
 
-		linha = arquivo.readline()#CASOS
-		linha = arquivo.readline().replace('\n','') #número de casos
-		doencas[-1][-1] = linha.replace('\n','')
+#Adicionar na base de dados
+cursor.execute('delete from doencas')
+cursor.execute('delete from sintomas')
+cursor.execute('delete from relacoes')
+for d in doencas:
+	cursor.execute("insert into doencas values(null, '%s', '%s')" %(d[0], d[1]))
 
-		c.execute("insert into doencas values(null, '%s', '%s')" %(doencas[-1][0], doencas[-1][1]))
+for s in sintomas:
+	cursor.execute("insert into sintomas values(null, '%s')" %s)
 
-		linha = arquivo.readline() #SINTOMAS
-		linha = arquivo.readline().replace('\n','') #primeiro sintoma
+for r in range(len(relacoes)):
+	cursor.execute("select id from doencas where nome='%s'" %relacoes[r][0])
+	for linha in cursor:
+		id_doenca = linha[0]
 
-		while '===' not in linha:
-			if linha not in sintomas:
-				sintomas.append(linha)
-				c.execute("insert into sintomas values(null, '%s')" %sintomas[-1])
+	cursor.execute("select id from sintomas where nome='%s'" %relacoes[r][1])
+	for linha in cursor:
+		id_sintoma = linha[0]
 
-			inserirRelacao(doencas[-1][0], sintomas[-1])
+	cursor.execute("insert into relacoes values(null, %d, %d)" %(id_doenca, id_sintoma))
 
-			linha = arquivo.readline().replace('\n','') #próximos sintomas
-		
-		linha = arquivo.readline() #NOME
-
-arquivo = abre_arquivo()
-le_arquivo(arquivo)
-conn.commit()
-c.close()
+database.commit()
+cursor.close()
 arquivo.close()
